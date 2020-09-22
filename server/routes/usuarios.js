7dc37e3302733a82dba25d1e6f2e257a792425fb
Myriam <mrodenas@santandertecnologia.com.ar>
2020-09-22 17:11:04 -0300

    Login normal seccion 10

--- server/routes/usuarios.js
+++ server/routes/usuarios.js
@@ -1,11 +1,22 @@
 const express = require('express');
 const bcrypt = require('bcrypt');
 const _ = require('underscore');
+
 const Usuario = require('../models/usuario');
+const { verificaToken } = require('../middlewares/autenticacion');
+const { verificaAdmin_Role } = require('../middlewares/autenticacion');
+
 const app = express();
 
-app.get('/usuario', function(req, res) {
+app.get('/usuario', verificaToken, (req, res) => {
     //    res.json('get Usuario LOCAL');
+
+    // return res.json({
+    //     usuario: req.usuario,
+    //     nombre: req.usuario.nombre,
+    //     email: req.usuario.email
+    // });
+
     let desde = req.query.desde || 0;
     desde = Number(desde);
 
@@ -32,7 +43,8 @@ app.get('/usuario', function(req, res) {
         });
 });
 
-app.post('/usuario', function(req, res) {
+app.post('/usuario', [verificaToken, verificaAdmin_Role], (req, res) => {
+
     let body = req.body;
     let usuario = new Usuario({
         nombre: body.nombre,
@@ -54,7 +66,8 @@ app.post('/usuario', function(req, res) {
         });
     });
 });
-app.put('/usuario/:id', function(req, res) {
+
+app.put('/usuario/:id', [verificaToken, verificaAdmin_Role], (req, res) => {
     let id = req.params.id;
     let body = _.pick(req.body, ['nombre', 'email', 'img', 'role', 'estado']);
     Usuario.findByIdAndUpdate(id, body, { new: true, runValidators: true }, (err, usuarioDB) => {
@@ -71,7 +84,7 @@ app.put('/usuario/:id', function(req, res) {
     })
 });
 
-app.delete('/usuario/:id', function(req, res) {
+app.delete('/usuario/:id', [verificaToken, verificaAdmin_Role], (req, res) => {
     let id = req.params.id;
 
     //    Usuario.findByIdAndRemove(id, (err, usuarioBorrado) => {
@@ -101,4 +114,5 @@ app.delete('/usuario/:id', function(req, res) {
         });
     });
 });
+
 module.exports = app;
--- server/server.js
+++ server/server.js
@@ -1,13 +1,19 @@
 require('./config/config');
+
 const mongoose = require('mongoose');
 const express = require('express');
+
 const app = express();
+
 const bodyParser = require('body-parser');
+
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
-    // parse application/json
+
+// parse application/json
 app.use(bodyParser.json())
-app.use(require('./routes/usuarios'));
+
+app.use(require('./routes/index'));
 // await mongoose.connect('mongodb://localhost/27017/cafe', {
 //     useNewUrlParser: true,
 //     useUnifiedTopology: true,
@@ -17,11 +23,13 @@ app.use(require('./routes/usuarios'));
 //     if (err) throw err;
 //     console.log('Base de datos ONLINE');
 // });
+
 mongoose.connect(process.env.URLDB, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true },
     (err, res) => {
         if (err) throw err;
         console.log('Base de datos ONLINE');
     });
+
 app.listen(process.env.PORT, () => {
     console.log('Escuchando puerto: ', process.env.PORT);
 });
